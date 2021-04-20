bfd1fe0fedbc193804dedf446af0c2a4846b0ac2
NikoWLR <wahlroosniko@gmail.com>
2021-04-20 11:52:58 +0300

    Lisää kommenttien siivoamista

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 '''
 TODO: 
-Jako luokkiin
-Kommentit kuntoon
-Tallennusfunktio
+Jako luokkiin (done ainakin osittain)
+Kommentit kuntoon (done)
+Tallennusfunktio (WIP)
+Yleistä hienosäätöä
 
 
 '''
@@ -52,73 +53,39 @@ def delete(taskNumberField, clear_taskNumberField, TextArea) :
     # Mikä tehtävä poistetaan
     number = taskNumberField.get(1.0, END)
  
-    # checking for input error when
-    # empty input in task number field
+    # Jos tyhjä
     if number == "\n" :
         messagebox.showerror("input error")
-        return
-     
+        return    
     else :
         task_no = int(number)
-
     clear_taskNumberField()
      
-    # deleted specified task from the list
+    # poistaa pyydetyn tehtävän
     tasks_list.pop(task_no - 1)
- 
-    # decremented
     counter -= 1
-     
-    # whole content of text area widget is deleted
     TextArea.delete(1.0, END)
  
-    # rewriting the task after deleting one task at a time
+    # kirjoittaa tehtävälistan uudelleen kun tehtävä on poistettu
     for i in range(len(tasks_list)) :
         TextArea.insert('end -1 chars', "[ " + str(i + 1) + " ] " + tasks_list[i])
      
-class gui(): 
-# Driver code
+class gui(): #GUI- luokka. Sisältää tkinter säädöt ja muut mahdolliset Ui elementit #TODO: Siirrä omaan tiedstoonsa
     if __name__ == "__main__" :
- 
         gui = Tk()
         gui.configure(background = "light gray")
         gui.title("ToDo")
         gui.geometry("250x300")
- 
-    # create a label : Enter Your Task
         enterTask = Label(gui, text = "Enter Your Task", bg = "light gray")
- 
-    # create a text entry box
-    # for typing the task
         enterTaskField = Entry(gui)
- 
-    # create a Submit Button and place into the root window
-    # when user press the button, the command or
-    # function affiliated to that button is executed
-
         Submit = Button(gui, text = "Submit", fg = "Black", bg = "gray", command = insertTask)
- 
-    # create a text area for the root
-    # with lunida 13 font
-    # text area is for writing the content
-        TextArea = Text(gui, height = 5, width = 25, font = "lucida 13")
- 
-    # create a label : Delete Task Number
-        taskNumber = Label(gui, text = "Delete Task Number", bg = "light gray")
-                        
+        TextArea = Text(gui, height = 5, width = 25, font = "lucida 13") #tekstikenttä sisällön kirjoittamiseen
+        taskNumber = Label(gui, text = "Delete Task Number", bg = "light gray")                        
         taskNumberField = Text(gui, height = 1, width = 2, font = "lucida 13")
- 
-    # create a Delete Button and place into the root window
-    # when user press the button, the command or
-    # function affiliated to that button is executed .
         delete = Button(gui, text = "Delete", fg = "Black", bg = "gray", command = delete)
- 
-    # create a Exit Button and place into the root window
         Exit = Button(gui, text = "Exit", fg = "Black", bg = "gray", command = exit)
  
-    # grid method is used for placing
-    # the widgets at respective positions
-    # in table like structure.
+    # ruudukon parametrit
         enterTask.grid(row = 0, column = 2)             
         enterTaskField.grid(row = 1, column = 2, ipadx = 50)                      
         Submit.grid(row = 2, column = 2)
@@ -129,5 +96,5 @@ class gui():
                         
         Exit.grid(row = 7, column = 2)
  
-    # start the GUI
+    # käynnistää GUI:n
         gui.mainloop()
