9dbacded0dc958da8e03478a3b7c4bb7d8407efe
NikoWLR <wahlroosniko@gmail.com>
2021-04-22 17:30:42 +0300

    Vikat muutokset, kommenttien hienosäätöä

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 '''
-#TODO: 
-Jako luokkiin (done ainakin osittain)
-Kommentit kuntoon (done)
-Tallennusfunktio (erittäin WIP)
-Yleistä hienosäätöä
-Paperityöt & raportit (Working on it)
+@author: Niko Wahlroos
+Palautuspäivä: 22/04/2021
+
+Olio-ohjelmoinnni loppuprojekti.
+ToDo-sovellus, jossa käyttäjä pystyy (ainakin teoriassa) tallentamaan haluamansa tehtävän ja myöhemin tehtävän suoritettuaan poistamaan sen.
+
 '''
 
 #Importtaukset
@@ -14,8 +14,8 @@ from tkinter import messagebox
 from tiedostohandler import *
  
 # lista, johon tehtävät tallennetaan
-tasks_list = [] #
-counter = 1
+tasks_list = [] # Kyseessä lista indeksoinnin automatisoinnin vuoksi
+counter = 1 # laskee tehtävien määrän
 
 
 class main: #Sovelluksen pääluokka. Sisältää päälogiikan
@@ -27,9 +27,9 @@ class main: #Sovelluksen pääluokka. Sisältää päälogiikan
         self.tekstiAlue = tekstiAlue
         self.tasks_list = []
         self.counter = counter
-        print ("toimii tähän asti")
 
-    def inputError(self, enterTaskField) :     # jos input error -> ilmoita
+
+    def inputError(self, enterTaskField) :     # jos error -> ilmoita käyttäjälle
         if enterTaskField.get() == "" :
             messagebox.showerror("Input Error")   
             return 0
@@ -41,7 +41,7 @@ class main: #Sovelluksen pääluokka. Sisältää päälogiikan
     def clear_taskField(self, enterTaskField) :# tehtäväkentän tyhjennys
         enterTaskField.delete(0, END)
  
-    def lisaaTehtava(self): # tehtävän lisääminen #? ongelma toivottavasti täällä
+    def lisaaTehtava(self): # Uuden tehtävän lisääminen
         
         global counter
         value = self.inputError() 
@@ -49,23 +49,23 @@ class main: #Sovelluksen pääluokka. Sisältää päälogiikan
             return
         content = self.enterTaskField.get() + "\n"
  
-    # tallentaa tehtävän listaan
+    # Tallentaa tehtävän listaan
         tasks_list.append(content) 
     # lisää tehtävän sovelluksen tekstikenttään
         self.tekstiAlue.insert('end -1 chars', "[ " + str(counter) + " ] " + content)
         counter += 1
         self.clear_taskField()
 
-    def delete(self, taskNumberField, clear_taskNumberField, tekstiAlue):    # funktio tehtävien poistamiseen
+    def delete(self, taskNumberField, clear_taskNumberField, tekstiAlue):    # Tehtävien poistaminen
         global counter    
         if len(tasks_list) == 0 :      # Jos tyhjä
             messagebox.showerror("404 tehtävää ei löytynyt")
             return
     
-        # Mikä tehtävä poistetaan
+        # Mikä tehtävä poistetaan?
         number = self.taskNumberField().get(1.0, END)
     
-        # Jos tyhjä
+        # Jos tyhjä -> ilmoita käyttäjälle
         if number == "\n" :
             messagebox.showerror("input error")
             return    
@@ -84,36 +84,11 @@ class main: #Sovelluksen pääluokka. Sisältää päälogiikan
 
     
 
-class SaveAndLoad(): #WIP. Tähän tulee lataus/tallennus systeemit. Tod.näk kopioitu häpeilemättä jostain labratyöstä #TODO: Siirrä omaan tiedostoon
-    def __init__(self):
-        try:
-            self.edellinen = TiedostoHandler().lue('rivi.dat').split(',')
-            if len(self.edellinen) >= 3: #tyyppi, määrä ja ainakin yhden nopan silmäluku
-                self.noppa.typpi = int(self.edellinen[0])
-                self.noppa.maara = int(self.edellinen[1])
-                self.entry.insert(0, str(self.noppa.maara)) #jatketaan edellisestä
-                s =''
-                for i, item in enumerate(self.edellinen[2:]):
-                    if item: #ei tyhjiä
-                       s += (str(item) + ', ') if i < len(self.edellinen[2:]) -1 else str(item)
-                tk.messagebox.showinfo('Edellinen heitto',f'Noppa tyyppi {self.noppa.typpi}: {s}')
-        except:
-             tk.messagebox.showinfo('Edellistä heittoa ei voitu hakea', 'Tee uusi heitto') 
-        
-        self.protocol("WM_DELETE_WINDOW", self.close)
-        pass
-
-    def close(self):
-        s =''
-        for i, item in enumerate(self.edellinen):
-            if item: #ei tyhjiä
-                s += (str(item) + ', ') if i < len(self.edellinen) -1 else str(item)
-        TiedostoHandler().kirjoita('rivi.dat', s)
-        self.destroy()
-
+class SaveAndLoad(): #WIP. Tähän tulee lataus/tallennus systeemit. Tod.näk kopioitu häpeilemättä jostain labratyöstä #! Aika loppui = 404 class not found
+    pass
 
      
-class gui: #GUI- luokka. Sisältää tkinter säädöt ja muut mahdolliset Ui elementit #TODO: Siirrä omaan tiedstoonsa
+class gui: #GUI- luokka. Sisältää tkinter säädöt ja muut mahdolliset Ui elementit. Käyttäjä avaa vain yhden sovellusikkunan, eli gui-elementtien luokassa sijaitseminen ei erityisemmin haittaa
 
     def __init__(self):
         self.gui = gui
@@ -121,20 +96,21 @@ class gui: #GUI- luokka. Sisältää tkinter säädöt ja muut mahdolliset Ui el
         self.inputError = main.inputError
         self.delete = main.delete
 
+    #UI:n logiikka (napit yms.)
     gui = Tk()
-    gui.configure(background = "light gray")
-    gui.title("ToDo")
-    gui.geometry("250x300")
-    enterTask = Label(gui, text = "Lisää tehtävä", bg = "light gray")
-    enterTaskField = Entry(gui)
+    gui.configure(background = "light gray") # taustan väri
+    gui.title("ToDo") # ikkunan otsikko/nimi
+    gui.geometry("250x300") # ikkunan koko
+    enterTask = Label(gui, text = "Lisää tehtävä", bg = "light gray") # tekstiä
+    enterTaskField = Entry(gui) # tekstikenttä itse tehtävälle
     Submit = Button(gui, text = "Lisää", fg = "Black", bg = "gray", command = main.lisaaTehtava) # submit-nappi
     tekstiAlue = Text(gui, height = 5, width = 25, font = "lucida 13") #tekstikenttä sisällön kirjoittamiseen
-    taskNumber = Label(gui, text = "Poistettavan tehtävän numero:", bg = "light gray")                        
-    taskNumberField = Text(gui, height = 1, width = 2, font = "lucida 13")
-    main.delete = Button(gui, text = "Poista", fg = "Black", bg = "gray", command = main.delete)
-    Exit = Button(gui, text = "Exit", fg = "Black", bg = "gray", command = exit)
+    taskNumber = Label(gui, text = "Poistettavan tehtävän numero:", bg = "light gray") # lisää tekstiä                       
+    taskNumberField = Text(gui, height = 1, width = 2, font = "lucida 13") # tekstikenttä tehtävän numerolle
+    main.delete = Button(gui, text = "Poista", fg = "Black", bg = "gray", command = main.delete) # delete - nappi
+    Exit = Button(gui, text = "Exit", fg = "Black", bg = "gray", command = exit) # exit - nappi
  
-    # ruudukon parametrit
+    # ruudukon parametrit & UI-elementit
     enterTask.grid(row = 0, column = 2)             
     enterTaskField.grid(row = 1, column = 2, ipadx = 50)                      
     Submit.grid(row = 2, column = 2)
--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-from main import gui
-from main import *
+from main import gui # tuo main-moduulista gui-luokan
 
-if __name__ == "__main__" :
+if __name__ == "__main__" : # ajaa tkinter main loopin ja käynnistää itse ohjelman
     gui.gui.mainloop()
